bec077b9a332872e6a7c8871647fa470c0891636
yukiyukivirtual <fegi3.4+yukiyuki@gmail.com>
2018-05-31 20:10:17 +0900

    本当はやりたくなかったけどサンプルコピペ

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
 (function()
 {
-	navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-   getUserMedia: function(c) {
-     return new Promise(function(y, n) {
-       (navigator.mozGetUserMedia ||
-        navigator.webkitGetUserMedia).call(navigator, c, y, n);
-     });
-   }
-} : null);
-
-if (!navigator.mediaDevices) {
-  console.log("getUserMedia() not supported.");
-  return;
-}
-
-	// カメラの設定
-	var media = { audio: true, video: { width: 1280, height: 720 } };/*{
-		audio : false,
-		video : {
+	const medias = {
+		audio : false, video : {
 			facingMode : {
 				exact : "environment"
 			}
 		}
-	};*/
+	};
+	const video = document.getElementById("video");
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
 
-	// カメラ起動
-	navigator.mediaDevices.getUserMedia(media)
-	.then(function(stream) {	// 成功
-		var video = document.getElementById("video");	// カメラ映像表示先
-		video.src = window.URL.createObjectURL(stream);	// カメラ映像ソース
-		video.onloadedmetadata = function(e) {
-			video.play();	// 動画再生
-		};
-	})
-	.catch(function(err) {	// 失敗
-		alert(err.name + ": " + err.message);
-	});
+	navigator.getUserMedia(medias, successCallback, errorCallback);
+
+	requestAnimationFrame(draw);
+
+	function successCallback(stream) {
+		video.srcObject = stream;
+	};
+
+	function errorCallback(err) {
+		alert(err);
+	};
+
+	function draw() {
+		canvas.width  = window.innerWidth;
+		canvas.height = window.innerHeight;
+		ctx.drawImage(video, 0, 0);
+
+		requestAnimationFrame(draw);
+	}
 })();
